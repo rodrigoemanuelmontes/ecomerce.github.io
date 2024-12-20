263aa2ea3be0815fd5da5d635b3aa2636e8cc6ab
Rodrigo Montes <rodrigomontes167@gmail.com>
2024-12-20 20:41:07 -0300

    cambio la ruta de productos json para ver si ese es el problema

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Función para cargar el archivo JSON con los productos
 function cargarProductos() {
-    fetchfetch('./productos.json')
+    fetchfetch('/productos.json')
         .then(response => response.json())
         .then(data => {
             productos = data;  
